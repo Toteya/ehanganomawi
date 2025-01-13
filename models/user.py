cfab039405683617cf61a2adf70fa2c13ecc780a
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-13 18:16:16 +0200

    new model: user account

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+module user: Contains user account implementation
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    A user account
+    """
+    pass
