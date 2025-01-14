a64e4aa0c3fc9b35b7110cb1ec464bc92536daf5
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-14 18:25:33 +0200

    initialise test_models package

---
+++ tests/test_models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Initialises the test_models package
+"""
+from os import environ
+
+environ['OMAWI_MYSQL_USER'] = 'omawi_test'
+environ['OMAWI_MYSQL_PWD'] = 'omawi_test_pwd'
+environ['OMAWI_MYSQL_HOST'] = 'local_host'
+environ['OMAWI_MYSQL_DB'] = 'omawi_test_db'
+environ['OMAWI_ENV'] = 'test'
+
