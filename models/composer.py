11335f9c2ad4a52c15be6131d115a43b66b7ae09
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-15 01:50:13 +0200

    new model: Composer

---
+++ models/composer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+module composer:
+Contains composer model implementation
+"""
+from models.base_model import Base, BaseModel, Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+
+class Composer(BaseModel, Base):
+    """
+    A composer of a melody
+    """
+    __tablename__ = 'composers'
+
+    name = Column('name', String(45))
+
+    melodies = relationship('Melody', back_populates='composer')
