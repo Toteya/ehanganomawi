f8b009c64997024e5436a2d55375bfbc60d07fd1
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-14 06:41:03 +0200

    new model: playlist

---
+++ models/playlist.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+module playlist: implements user playlist
+"""
+from models.base_model import BaseModel
+
+
+class Playlist(BaseModel):
+    """
+    A plaaylist
+    """
+    name = ''
+    user_id = ''
+    hymns = None
