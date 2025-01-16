67ab1e6430a21defdf04a3d5981c0a9298e14de8
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-16 21:48:43 +0200

    modified test_app: update flask configuration variable CONFIG_TYPE before creating app

--- tests/test_api/test_v1/test_app.py
+++ tests/test_api/test_v1/test_app.py
@@ -9,6 +9,8 @@ from api.v1.app import create_app
 import pytest
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
 app = create_app()
 
 @pytest.fixture
