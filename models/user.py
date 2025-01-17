071d49a23307d6b930ab95b9a021abd19caedbdf
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-17 22:35:15 +0200

    updated user model to allow for session management using flask_login library; modified email column contraints -> unique

--- models/user.py
+++ models/user.py
@@ -4,14 +4,15 @@ module user: Contains user account implementation
 """
 from models.base_model import Base, BaseModel, Column
 from sqlalchemy import String
+from flask_login import UserMixin
 
 
-class User(BaseModel, Base):
+class User(BaseModel, UserMixin, Base):
     """
     A user account
     """
     __tablename__ = 'users'
 
     name = Column('name', String(60))
-    email = Column('email', String(60))
+    email = Column('email', String(60), unique=True)
     password = Column('password', String(255))
