68b0d5ed63bb547212b6c922094a9c35c738fa05
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-17 22:40:12 +0200

    added protection to profile page -> login is required to access; template displays the current users name instead of generic greeting

--- web_app/main/main.py
+++ web_app/main/main.py
@@ -5,6 +5,7 @@ Renders main web app templates
 """
 from web_app.main import app_main
 from flask import render_template
+from flask_login import login_required, current_user
 
 @app_main.route('/')
 def index():
@@ -13,7 +14,8 @@ def index():
     return render_template('index.html')
 
 @app_main.route('/profile')
+@login_required
 def profile():
     """ Renders profile page
     """
-    return render_template('profile.html')
+    return render_template('profile.html', name=current_user.name)
