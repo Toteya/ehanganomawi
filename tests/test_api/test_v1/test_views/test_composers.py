e39008a099fa956e1a5cde7a04d20272e04e0b2a
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-18 12:15:13 +0200

    tests for the composers endpoint

---
+++ tests/test_api/test_v1/test_views/test_composers.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+module test_composers:
+Contains pytests for API views - composers endpoints
+"""
+from test_app import client
+import sys
+import os
+import pytest
+from models import storage
+from models.composer import Composer
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+@pytest.fixture(scope='module')
+def new_composer():
+    composer1 = Composer(name='Sibelius')
+    storage.new(composer1)
+    storage.save()
+    yield
+    storage.delete(composer1)
+    storage.save()
+
+def test_composers(client, new_composer):
+    """ Tests that composers route returns all existing composers
+    """
+    response = client.get('/api/v1/composers')
+    assert response.status_code == 200
+    assert len(response.json) == 1
