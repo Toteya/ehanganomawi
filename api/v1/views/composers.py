6dddf7c7ea09046274483256db930c6befa06b7b
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-18 12:14:10 +0200

    new endpoint: composers - returns a JSON list of all the existing composers in the database

---
+++ api/v1/views/composers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+module composers:
+Contains API endpoints related to composer objects
+"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.composer import Composer
+
+@app_views.route('/composers', strict_slashes=False)
+def composer():
+    """ Returns all the composers in the database
+    """
+    composers = storage.all(Composer).values()
+    composers_list = [composer.to_dict() for composer in composers]
+    return jsonify(composers_list)
