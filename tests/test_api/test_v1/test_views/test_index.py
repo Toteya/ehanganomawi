7e507206c93f6b0e1a9c0c134a60aeb9c4e446b4
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-18 13:08:33 +0200

    pytests for the newly added api endpoint

--- tests/test_api/test_v1/test_views/test_index.py
+++ tests/test_api/test_v1/test_views/test_index.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 """
-module test_index
-Tests API views - index
+module test_index:
+Contains pytests for API views - index endpoints
 """
+from models import storage
+from models.composer import Composer
+from models.hymn import Hymn
+from models.user import User
 from test_app import client
+import pytest
 import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+@pytest.fixture(scope='module')
+def create_objects():
+    """ Creates and saves objects to the test database
+    """
+    storage.new(Composer(name='Chopin'))
+    storage.new(Composer(name='Bach'))
+    storage.new(Hymn(number=23))
+    storage.new(User(name='Mike', email='mike@mail.com', password='myPass123'))
+    storage.new(User(name='Marty', email='marty@mail.com', password='secretPWD21'))
+    storage.save()
+    yield
+    objs = storage.all().values()
+    for obj in objs:
+        storage.delete(obj)
+    storage.save()
+
 
 def test_status(client):
     """ Test the status route
@@ -15,3 +37,13 @@ def test_status(client):
     response = client.get('/api/v1/status')
     assert response.status_code == 200
     assert response.json == {'Status': 'OK'}
+
+def test_stats(client, create_objects):
+    """ Test that the '/stats' route returns a summary of all existing objects
+    """
+    response = client.get('/api/v1/stats')
+    assert response.status_code == 200
+    assert 'Composers' in response.json
+    assert response.json.get('Composers') == 2
+    assert response.json.get('Hymns') == 1
+    assert response.json.get('Users') == 2
