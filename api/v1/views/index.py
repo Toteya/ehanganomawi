930e81ecd47029922b83d60ec0d11986444fa821
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-18 13:07:43 +0200

    new endpoint: stats - Returns a summary (quantities) of all the existing object in database

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ API index endpoints
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -12,4 +13,17 @@ def status():
     """
     return jsonify({"Status": "OK"})
 
-# implement stats endpoint
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ Returns statistic about all the existing objects in the daatabase
+    """
+    objs = storage.all()
+    objs_stats = {}
+    for key in objs.keys():
+        clss = key.split('.')[0] + 's'
+        if clss not in objs_stats:
+            objs_stats[clss] = 1
+        else:
+            objs_stats[clss] += 1
+
+    return jsonify(objs_stats)
