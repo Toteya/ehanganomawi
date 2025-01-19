4133afbf07289dac3cbfa26d11608f81e36c71ef
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-19 18:52:22 +0200

    Revert "modified page from template"
    This reverts commit 0d778c35a94ad28709abbf6d8304b50ab97410bb.

--- web_app/static/scripts/index.js
+++
@@ -1,9 +0,0 @@
-// Handles standard interactivy of web application
-$(document).ready(() => {
-  const burgerIcon = document.querySelector('#burger');
-  const navbarMenu = document.querySelector('#navbar-links');
-
-  burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active');
-  })
-});
