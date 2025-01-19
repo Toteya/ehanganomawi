742b75af2d2a21e6870f78a9a8f9499a3dad0879
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-19 12:24:33 +0200

    cosmetic changes: use more sensible function names; provide an id when creating test objects to allow for easier manipulation during tests

--- tests/test_api/test_v1/test_views/test_hymns.py
+++ tests/test_api/test_v1/test_views/test_hymns.py
@@ -10,11 +10,11 @@ import pytest
 
 
 @pytest.fixture(scope='module')
-def new_hymns():
+def create_hymns():
     """ Creates hymn objects for testing
     """
     hymn34 = Hymn(number=34, id='93016e68-8e7e')
-    hymn22 = Hymn(number=22)
+    hymn22 = Hymn(number=22, id='43870a5d-cbd0')
     storage.new(hymn34)
     storage.new(hymn22)
     storage.save()
@@ -24,14 +24,14 @@ def new_hymns():
     storage.save()
 
 
-def test_get_hymns(client, new_hymns):
+def test_get_hymns(client, create_hymns):
     """ Test that the endpoint returns all existing hymns in the database
     """
     response = client.get('/api/v1/hymns')
     assert response.status_code == 200
     assert len(response.json) == 2
 
-def test_get_hymn(client, new_hymns):
+def test_get_hymn(client, create_hymns):
     """ Test that the endpoint returns the correct hymn matching the given ID
     """
     response = client.get('/api/v1/hymns/93016e68-8e7e')
