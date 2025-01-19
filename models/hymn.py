e9d462442965b9f3ff4c8b9a223a811dd160c9d7
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-19 11:58:52 +0200

    removed unnecessary imports; removed unnecessary comments; added backref and cascades setting to allow for automatic deletion of child objects

--- models/hymn.py
+++ models/hymn.py
@@ -4,7 +4,7 @@ module hymn: Contains Hymn implementation
 """
 from models.base_model import Base, BaseModel, Column
 from models.melody import hymn_melody_assoc_table
-from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy import Integer
 from sqlalchemy.orm import relationship
 
 
@@ -15,8 +15,7 @@ class Hymn(BaseModel, Base):
     __tablename__ = 'hymns'
 
     number = Column('number', Integer, unique=True)
-    # melody_id = Column('melody_id', String(45), ForeignKey('melodies.id'))
 
-    verses = relationship('Verse')
+    verses = relationship('Verse', backref='hymn', cascade='all, delete-orphan')
     melodies = relationship('Melody', secondary=hymn_melody_assoc_table,
                             back_populates='hymns', viewonly=True)
