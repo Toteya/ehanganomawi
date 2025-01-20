e0fd2209dfd6fea77f98a6135bd4e1f255a9edc1
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-20 14:28:49 +0200

    updated media player muting control buttons - icons, interactivity, and functionality

--- web_app/static/scripts/songs.js
+++ web_app/static/scripts/songs.js
@@ -1,49 +1,84 @@
 // Handles the interactive functionality of the music player / songs page
 $(document).ready(() => {
-    const soprano = document.getElementById('soprano');
-    const alto = document.getElementById('alto');
-    const tenor = document.getElementById('tenor');
-    const bass = document.getElementById('bass');
+  const soprano = $('#soprano');
+  const alto = $('#alto');
+  const tenor = $('#tenor');
+  const bass = $('#bass');
 
-    const muteSoprano = document.getElementById('mute-soprano')
-    const muteAlto = document.getElementById('mute-alto')
-    const muteTenor = document.getElementById('mute-tenor')
-    const muteBass = document.getElementById('mute-bass')
+  const muteSoprano = $('#mute-soprano')
+  const muteAlto = $('#mute-alto')
+  const muteTenor = $('#mute-tenor')
+  const muteBass = $('#mute-bass')
 
-    muteSoprano.click(() => {
-        console.log('MUTE SOPRANO CLICKED!')
-        soprano.muted = !soprano.muted;
-        muteSoprano.classList.toggle('is-Active')
-    });
+  const items = [
+    {'audioItem': soprano, 'muteButton': muteSoprano},
+    {'audioItem': alto, 'muteButton': muteAlto},
+    {'audioItem': tenor, 'muteButton': muteTenor},
+    {'audioItem': bass, 'muteButton': muteBass},
+  ];
 
-    $('#mute-alto').click(function() {
-        // alto.muted = !alto.muted;
-        // $(this).classList.toggle('is-Active')
-        const button = $(this)[0];
-        button.classList.toggle('is-active');
-    });
+  const toggleMute = (audioItem, muteButton) => {
+    const icon = muteButton.find('i');
+    if (audioItem.muted) {
+      unmuteAudio(audioItem, icon);
+    } else {
+      muteAudio(audioItem, icon);
+    }
+  }
 
-    $('#mute-tenor').click(() => {
-        tenor.muted = !tenor.muted;
-        $(this).classList.toggle('is-Active')
-    });
+  const muteAudio = (audioItem, icon) => {
+    audioItem.muted = true;
+    icon.removeClass('fa-volume-high');
+    icon.addClass('fa-volume-xmark');
+  }
 
-    $('#mute-bass').click(() => {
-        bass.muted = !bass.muted;
-        $(this).classList.toggle('is-Active')
-    });
+  const unmuteAudio = (audioItem, icon) => {
+    audioItem.muted = false;
+    icon.removeClass('fa-volume-xmark');
+    icon.addClass('fa-volume-high');
+  }
 
-    $('#play').click(() => {
-        soprano.play();
-        alto.play();
-        tenor.play();
-        bass.play();
-    });
+  $('#mute-soprano').click(() => {
+    toggleMute(soprano, muteSoprano);
+  });
 
-    $('#pause').click(() => {
-        soprano.pause();
-        alto.pause();
-        tenor.pause();
-        bass.pause();
-    });
+  $('#mute-alto').click(() => {
+    toggleMute(alto, muteAlto);
+  });
+
+  $('#mute-tenor').click(() => {
+    toggleMute(tenor, muteTenor);
+  });
+
+  $('#mute-bass').click(() => {
+    toggleMute(bass, muteBass);
+  });
+
+  $('#unmute-all').click(() => {
+    for (const item of items) {
+      const icon = item.muteButton.find('i');
+      unmuteAudio(item.audioItem, icon);
+    }
+  });
+
+  $('#mute-all').click(() => {
+    for (const item of items) {
+      const icon = item.muteButton.find('i');
+      muteAudio(item.audioItem, icon);
+    }
+  });
+
+  $('#play').click(() => {
+    soprano.play();
+    alto.play();
+    tenor.play();
+    bass.play();
+  });
+
+  $('#pause').click(() => {
+    soprano.pause();
+    alto.pause();
+    tenor.pause();
+    bass.pause();
+  });
 });
