42ce511b3c8157903fee3f558fd931fe61a977e0
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-25 09:30:54 +0200

    modify getSongMelody function to return a Promise

--- web_app/static/scripts/requests.js
+++ web_app/static/scripts/requests.js
@@ -1,16 +1,21 @@
 // Handles API requests
 
 const getSongMelody = async (song_id, data = {}) => {
-  $.ajax({
-    type: 'GET',
-    url: `http://127.0.0.1:5001/api/v1/songs/${song_id}/melodies`,
-    data,
-    contentType: 'application/json',
-    success: (melodies) => {
-      // Temp: For now just return the first melody object if multiple melodies are found
-      return melodies[0];
-    }
-  });
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      type: 'GET',
+      url: `http://127.0.0.1:5001/api/v1/songs/${song_id}/melodies`,
+      data,
+      contentType: 'application/json',
+      success: (melodies) => {
+        // TO BE UPDATED: For now just return the first melody object in the array
+        resolve(melodies[0].filepath);
+      },
+      error: (error) => {
+        reject(error);
+      }
+    });
+  })
 }
 
 export { getSongMelody };
