d9da921b3423a7de07cf8c4b8506943c2d55cbf4
Toteya <111172685+Toteya@users.noreply.github.com>
2025-02-02 16:43:57 +0200

    tests for the new endpoint

--- tests/test_api/test_v1/test_views/test_songs.py
+++ tests/test_api/test_v1/test_views/test_songs.py
@@ -4,7 +4,9 @@ module test_songs:
 Contains pytests for API views - songs endpoints
 """
 from test_app import client
+from test_composers import create_composers
 from models import storage
+from models.melody import Melody
 from models.song import Song
 import pytest
 
@@ -31,12 +33,14 @@ def test_get_songs(client, create_songs):
     assert response.status_code == 200
     assert len(response.json) == 2
 
+
 def test_get_song(client, create_songs):
     """ Test that the endpoint returns the correct song matching the given ID
     """
     response = client.get('/api/v1/songs/93016e68-8e7e')
     assert response.status_code == 200
     assert response.json['number'] == 34
+
 
 def test_post_song(client, create_songs):
     """ Test that the endpoint correctly adds a song to the DB
@@ -61,3 +65,24 @@ def test_post_song(client, create_songs):
     data = {'title': 'Song 34', 'number': 34}
     response = client.post('/api/v1/songs', data=data)
     assert response.status_code == 400
+
+
+from test_melodies import create_melodies
+def test_get_song_by_composer(client, create_composers, create_melodies, create_songs):
+    """ Test that the endpoint returns all the songs whose melody is by
+    the given composer
+    """
+    composer_id = 'a6458a37-8b17'
+    song1 = storage.get(Song, '93016e68-8e7e')
+    song2 = storage.get(Song, '43870a5d-cbd0')
+    melody = storage.get(Melody, 'df7943f3-2759')
+    song1.melodies.append(melody)
+    song2.melodies.append(melody)
+
+    response = client.get(f'/api/v1/composers/{composer_id}/songs')
+    assert response.status_code == 200
+    songs = response.json
+    assert len(songs) == 2
+    song_titles = [song.get('title') for song in songs]
+    assert 'Song 22' in song_titles
+    assert 'Song 34' in song_titles
